ce9e91f46a2f67f8d480419412348fd501f8e084
Clementinox <Clementinox@users.noreply.github.com>
2014-09-30 21:31:36 -0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -15,7 +15,7 @@
 #
 
 ## (2) Also get non-open-source specific aspects if available
-$(call inherit-product-if-exists, vendor/lge/e975/e975-vendor.mk)
+$(call inherit-product-if-exists, vendor/lge/e970/e970-vendor.mk)
 
 ## overlays
 DEVICE_PACKAGE_OVERLAYS += device/lge/e970/overlay
@@ -29,19 +29,27 @@ $(call inherit-product, device/lge/ls970-common/ls970-common.mk)
 # Enable for debugging
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.debuggable=1 \
-    persist.service.adb.enable=1
+    persist.service.adb.enable=1 \
+    ro.secure=0 \
+    ro.adb.secure=0
 
 # Telephony Properties
 PRODUCT_PROPERTY_OVERRIDES += \
 	telephony.lteOnCdmaDevice=0 \
 	ro.telephony.default_network=9 \
-	telephony.lteOnGsmDevice=1
+	telephony.lteOnGsmDevice=1 \
+        ro.ril.def.preferred.network=9
 
 # Ramdisk
 PRODUCT_COPY_FILES += \
     device/lge/e970/ramdisk/init.e970.rc:root/init.e970.rc \
     device/lge/e970/ramdisk/ueventd.e970.rc:root/ueventd.e970.rc \
-    device/lge/e970/ramdisk/fstab.e970:root/fstab.e970
+    device/lge/e970/ramdisk/fstab.e970:root/fstab.e970 \
+    device/lge/e970/ramdisk/default.prop:root/default.prop
+    
+# TWRP Recovery
+PRODUCT_COPY_FILES += \
+    device/lge/e970/twrp.fstab:recovery/root/etc/twrp.fstab
 
 # These are the hardware-specific features
 PRODUCT_COPY_FILES += \
