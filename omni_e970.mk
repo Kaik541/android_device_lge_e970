bf444079ebb2f26a93a6ff3c59ec80301d6064b6
Clementinox <Clementinox@users.noreply.github.com>
2014-10-02 23:20:46 -0700

    Update omni_e970.mk

--- omni_e970.mk
+++ omni_e970.mk
@@ -6,7 +6,7 @@ TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 768
 
 # Inherit from the common Open Source product configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
